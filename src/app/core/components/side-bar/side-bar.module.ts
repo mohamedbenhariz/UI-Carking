fda1756e57400795bda1c0fada9a6c75011545e0
ULRICH POUANI CHOUAMENI SIEMENOU <pouani.ulrich19@myiuc.com>
2023-07-11 20:40:23 +0200

    mise sur pied du layout admin

--- src/app/core/components/side-bar/side-bar.module.ts
+++ src/app/core/components/side-bar/side-bar.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SideBarComponent } from './side-bar.component';
 import { SideBarHeaderComponent } from './side-bar-header/side-bar-header.component';
 import { SideMenuItemComponent } from './side-menu-item/side-menu-item.component';
+import {SvgModule} from "../../../shared/svg/svg.module";
 
 @NgModule({
     declarations: [
@@ -10,9 +11,12 @@ import { SideMenuItemComponent } from './side-menu-item/side-menu-item.component
         SideBarHeaderComponent,
         SideMenuItemComponent,
     ],
-    imports: [ CommonModule ],
     exports: [
-        SideBarComponent,
+      SideBarComponent,
+    ],
+    imports: [
+      CommonModule,
+      SvgModule
     ],
     providers: [],
 })
